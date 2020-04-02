a6b80f87b93b3442e6d987afcfadace9a20d60bb
Remi Gacogne <remi.gacogne@powerdns.com>
2020-04-02 16:49:44 +0200

    Fix an out-of-bounds read (up to 4 bytes) in the packet cache
    Detected by OSS-Fuzz. Also make sure that we don't try to parse
    packets smaller than 12 bytes in the fuzzing target, those are
    usually dropped earlier.

--- pdns/fuzz_packetcache.cc
+++ pdns/fuzz_packetcache.cc
@@ -27,7 +27,7 @@ StatBag S;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
-  if (size > std::numeric_limits<uint16_t>::max()) {
+  if (size > std::numeric_limits<uint16_t>::max() || size < sizeof(dnsheader)) {
     return 0;
   }
 
