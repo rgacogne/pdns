e08b748634313799d625163b5c33557cc04a456e
Remi Gacogne <remi.gacogne@powerdns.com>
2022-12-28 10:33:24 +0100

    rec: Implement basic stack overflow protection
    This works by adding two guard pages around the allocated
    stack memory, and marking these pages as neither readable
    nor writable, triggering an immediate access violation on
    any kind of access.

--- pdns/recursordist/lazy_allocator.hh
+++ pdns/recursordist/lazy_allocator.hh
@@ -31,6 +31,9 @@
 #define MAP_STACK 0
 #endif
 
+#define LAZY_ALLOCATOR_PROTECT 1
+#include <unistd.h>
+
 template <typename T>
 struct lazy_allocator
 {
@@ -43,11 +46,43 @@ struct lazy_allocator
   pointer
   allocate(size_type const n)
   {
+#ifdef LAZY_ALLOCATOR_PROTECT
+    /* This implements a very basic protection against stack overflow
+       by placing two guard pages around the requested memory: one
+       page right before the new stack and one right after.
+       The guard pages cannot be read or written to, any attempt to
+       do so will trigger an immediate access violation, terminating
+       the program.
+       This is much better than the default behaviour for two reasons:
+       1/ the program is stopped right before corrupting memory, which
+          prevents random corruption
+       2/ it's easy to find the point where the stack overflow occurred
+       The memory overhead is two pages (usually 4k on Linux) per stack,
+       and the runtime CPU overhead is one call to mprotect() for every
+       stack allocation.
+    */
+    static const auto pageSize = sysconf(_SC_PAGESIZE);
+
+    const size_type neededSize = n * sizeof(value_type);
+    void* p = mmap(nullptr, neededSize + (pageSize * 2),
+                   PROT_NONE, MAP_PRIVATE | MAP_ANON | MAP_STACK, -1, 0);
+    if (p == MAP_FAILED) {
+      throw std::bad_alloc();
+    }
+    void* usablePointer = static_cast<char*>(p) + pageSize;
+    int res = mprotect(usablePointer, neededSize, PROT_READ | PROT_WRITE);
+    if (res != 0) {
+      munmap(p, neededSize + (pageSize * 2));
+      throw std::bad_alloc();
+    }
+    return static_cast<pointer>(usablePointer);
+#endif /* LAZY_ALLOCATOR_PROTECT */
 #ifdef __OpenBSD__
     void* p = mmap(nullptr, n * sizeof(value_type),
                    PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_STACK, -1, 0);
-    if (p == MAP_FAILED)
+    if (p == MAP_FAILED) {
       throw std::bad_alloc();
+    }
     return static_cast<pointer>(p);
 #else
     return static_cast<pointer>(::operator new(n * sizeof(value_type)));
@@ -57,6 +92,14 @@ struct lazy_allocator
   void
   deallocate(pointer const ptr, size_type const n) noexcept
   {
+#ifdef LAZY_ALLOCATOR_PROTECT
+    static const auto pageSize = sysconf(_SC_PAGESIZE);
+
+    const size_type allocatedSize = (n * sizeof(value_type)) + (pageSize * 2);
+    void* basePointer = static_cast<char*>(ptr) - pageSize;
+    munmap(basePointer, allocatedSize);
+    return;
+#endif /* LAZY_ALLOCATOR_PROTECT */
 #ifdef __OpenBSD__
     munmap(ptr, n * sizeof(value_type));
 #else
