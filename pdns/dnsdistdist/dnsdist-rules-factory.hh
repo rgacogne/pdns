3c76d3c0dc8316af5144db7aacfb640718823fc0
Remi Gacogne <remi.gacogne@powerdns.com>
2025-08-26 14:05:33 +0200

    dnsdist: Slight speed up for the `PoolAvailable` rule
    We now have a slightly faster method to check whether a given pool
    has at least one server available, use it.
    
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

--- pdns/dnsdistdist/dnsdist-rules-factory.hh
+++ pdns/dnsdistdist/dnsdist-rules-factory.hh
@@ -1178,7 +1178,7 @@ public:
   bool matches(const DNSQuestion* dnsQuestion) const override
   {
     (void)dnsQuestion;
-    return (getPool(d_poolname)->countServers(true) > 0);
+    return (getPool(d_poolname)->hasAtLeastOneServerAvailable());
   }
 
   string toString() const override
